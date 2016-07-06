3b65a98b44154748827ea08edce65510be5fdc09
TakiyaHideto <hideto.jiahao.dong@gmail.com>
2016-07-06 17:38:19 +0800

    commit

--- ele.me/CountScoreFresh.py
+++ ele.me/CountScoreFresh.py
@@ -4,6 +4,9 @@ import ParseJson as pj
 class CountScoreFresh:
     def __init__(self, inputFile):
         self.inputFile = inputFile
+        self.scoreFreshDict = {}
+        self.scoreMax = 0.0
+        self.scoreMin = 0.0
 
     def __loadData(self):
         with open(self.inputFile, "r") as fileIn:
@@ -13,10 +16,26 @@ class CountScoreFresh:
                 jsonObject = pj.parseJson(line)
                 for key in jsonObject.keys():
                     foodName = key
-                    print foodName
+                    preferScore = round(jsonObject[foodName]["score"],2)
+                    isFresh = jsonObject[foodName]["is_fresh"]
+                    self.__putInfoIntoDict(self.scoreFreshDict, key="{0}_{1}".format(preferScore, isFresh))
+                    self.scoreMax = max(self.scoreMax, preferScore)
+                    self.scoreMin = min(self.scoreMin, preferScore)
+
+    def __putInfoIntoDict(self, dict, key):
+        if key in dict:
+            dict[key] += 1
+        else:
+            dict[key] = 1
+
+    def __pritnInfo(self):
+        print "max:{0}".format(self.scoreMax)
+        print "min:{0}".format(self.scoreMin)
+        print self.scoreFreshDict
 
     def runMe(self):
         self.__loadData()
+        self.__pritnInfo()
 
 if __name__ == "__main__":
     inputFile = "/home/jiahao.dong/test_file"
